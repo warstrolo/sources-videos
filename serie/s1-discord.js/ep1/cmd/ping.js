38a174722137ed18bbae13b373c4580f3e5c1953
warstrolo <warstrolo.wizard@gmail.com>
2021-04-26 17:10:53 +0200

    episode 1

--- serie/s1-discord.js/ep1/cmd/ping.js
+++ serie/s1-discord.js/ep1/cmd/ping.js
@@ -1,8 +1,7 @@
 module.exports = {
-    name : 'ping',
-    description : "Ping!",
+    name : "ping",
+    description : "ping",
     execute(message, args) {
-        message.channel.send('Pong.')
-
+        message.channel.send("Pong!")
     }
 }
--- serie/s1-discord.js/ep1/index.js
+++ serie/s1-discord.js/ep1/index.js
@@ -1,16 +1,17 @@
-const token = "ODM1OTAwODQwNjI2NzQ5NDcw.YIWLUg.cXWM1aQdf3w8BMbogaSoBuzT6E0"
-const prefix = "!"
-const Discord = require("discord.js")
-const client = new Discord.Client()
-const fs = require("fs")
-client.cmd = new Discord.Collection()
 
+
+const Discord = require('discord.js')
+const client = new Discord.Client()
+client.cmd = new Discord.Collection()
+const fs = require('fs')
+const prefix = "!"
 client.on('ready', () => {
     console.log("coucou je suis online")
     const command = fs.readdirSync('./cmd').filter(file => file.endsWith('.js'))
     for (const file of command)
     {
         const command = require(`./cmd/${file}`)
+
         client.cmd.set(command.name, command)
     }
 })
@@ -18,14 +19,17 @@ client.on('message', message => {
     if (!message.content.startsWith(prefix)) return
 
     const args = message.content.slice(prefix.length).trim().split(/ +/)
-    const command = args.shift().toLocaleLowerCase()
+    const command = args.shift().toLocaleLowerCase();
+
     if (!client.cmd.has(command)) return;
 
     try {
-        client.cmd.get(command).execute(message, args);
-    } catch(error) {
-        console.log(error)
-        message.reply("Error")
+        client.cmd.get(command).execute(message, args)
     }
+    catch (error) {
+        console.error(error)
+        message.reply('Erreur')
+    }
+
 })
-client.login(token)
+client.login("ODM2MjUzODc1Njc3NjkxOTc2.YIbUHA.uNdQWQFxcmFcFKkRj5sNuQABklk")
